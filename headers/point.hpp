80b390ea5cd4d45d067fab671f9cd97fdb9c2616
NpTerra <npterra000@gmail.com>
2025-04-28 23:42:53 +0200

    Point docs

--- headers/point.hpp
+++ headers/point.hpp
@@ -5,37 +5,106 @@
 
 class Point {
     private:
+        /**
+         * A síkon elhelyezkedő pontok összehasonlítása. Első sorban X, majd Y koordináta alapján.
+         * 
+         * \param p A másik pont, amivel össze kell hasonlítani.
+         * 
+         * \returns math2d::cmp(x1, x2), ha x1 != x2, egyébként math2d::cmp(y1, y2) 
+        */
         int cmp(const Point& p) const;
     public:
         double x, y;
         
-        static const double MAXVAL;
-        static const double MINVAL;
+        static const double MAXVAL; ///< X és Y értékek minimum határa
+        static const double MINVAL; ///< X és Y értékek maximum határa
 
         Point(double x = 0.0, double y = 0.0);
 
+        /**
+         * Két pont egyenlőségének vizsgálata.
+         * 
+         * \param p A másik pont.
+         * 
+         * \returns Igaz érték, ha a két pont X és Y koordinátái is megegyeznek.
+         */
         bool operator==(const Point& p) const;
         
+        /**
+         * Jelenlegi pont felülírása egy másik megadott ponttal.
+         * 
+         * \param p A másik pont.
+         * 
+         * \returns A módosított pont.
+         */
         Point& operator=(const Point& p);
 
+        /**
+         * Két pont összege.
+         * 
+         * \param p A másik pont.
+         * 
+         * \returns Egy új pont, ahol X és Y is a megfelelő koordináták összege.
+         */
         Point operator+(const Point& p) const;
+
+        /**
+         * Két pont különbsége.
+         * 
+         * \param p A másik pont.
+         * 
+         * \returns Egy új pont, ahol X és Y is a megfelelő koordináták különbsége.
+         */
         Point operator-(const Point& p) const;
 
         /**
-         * Distance
+         * Meghatározza két pont távolságát.
+         * 
+         * \param p A másik pont.
+         * 
+         * \returns A két pont távolsága.
          */
         double operator|(const Point& p) const;
+
         /**
-         * Dot Product
+         * Meghatározza két pont skaláris szorzatát. (dot product)
+         * 
+         * \param p A másik pont.
+         * 
+         * \returns A két pont skaláris szorzata.
          */
         double operator*(const Point& p) const;
+
         /**
-         * Cross Product
+         * Meghatározza két pont vektoriális szorzatát. (cross product)
+         * 
+         * \param p A másik pont.
+         * 
+         * \returns A két pont vektoriális szorzata.
          */
         double operator%(const Point& p) const;
         
+        /**
+         * Adatok beolvasása megadott bemenetről egy megadott pontba.
+         * 
+         * \param is A bemenet, amiről olvasni kell.
+         * \param p A pont, amibe az adatokat be kell olvasni.
+         * 
+         * \returns A bemenet referenciája, hogy lehetővé tegye az operátor láncolását.
+         */
         friend std::istream& operator>>(std::istream& is, Point& p);
 
+        /**
+         * @brief Összehasonlító operátorok definiálása makróval.
+         *
+         * A következő operátorok lesznek definiálva:
+         * - operator>
+         * - operator<
+         * - operator==
+         * - operator>=
+         * - operator<=
+         * - operator!=
+         */
         #define Comp(x) bool operator x (Point q) const;
             Comp(>) Comp(<) Comp(==) Comp(>=) Comp(<=) Comp(!=)
         #undef Comp
